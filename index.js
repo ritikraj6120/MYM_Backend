5cd4de7feac6dcbd21cb3b0d555284849fc943c0
Ritik Raj <ritikraj6120@gmail.com>
2023-07-03 23:33:57 +0530

    resolving cors 8th time

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require("path");
 var cors = require("cors");
 const app = express();
 var port = process.env.PORT || 5000;
+// app.options('*', cors())
+app.options('*', (req, res) => {
+    // Set CORS headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+  
+    // Respond with a 200 OK status
+    res.status(200).send();
+  });
+
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
